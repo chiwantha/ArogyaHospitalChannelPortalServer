85d44ea159b901eec9b8ab34c4ddc9fcc445d762
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2025-07-17 23:10:04 +0530

    20250717 finished dashboard count cards

--- controller/dashboards.js
+++ controller/dashboards.js
@@ -6,7 +6,10 @@ export const loadCounts = (req, res) => {
     (SELECT COUNT(id) FROM appointment WHERE state=1 AND is_confirmed = 1) AS approved,
     (SELECT COUNT(id) FROM appointment WHERE state=0 AND is_confirmed = 0) AS reject,
     (SELECT COUNT(id) FROM appointment ) AS total,
-    (SELECT COUNT(id) FROM doctors) AS doctors`;
+    (SELECT COUNT(id) FROM doctors WHERE state=1) AS doctors,
+    (SELECT COUNT(d.id) FROM doctors d LEFT JOIN session s ON d.id = s.doctor_id WHERE d.state=1 AND s.doctor_id IS NULL) AS doctors_no_session,
+    (SELECT COUNT(id) FROM session WHERE state=1) AS active_stssion,
+    (SELECT COUNT(id) FROM appointment WHERE state=1 AND is_confirmed = 1) * 100 AS sale`;
 
   db.query(query, (err, data) => {
     if (err) return res.status(500).json(err);
